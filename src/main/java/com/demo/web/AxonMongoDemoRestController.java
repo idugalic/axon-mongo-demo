7bf13c2738554fb9fb18b22dc903e428f6ebf3d9
Ivan Dugalic <ivan.dugalic@axoniq.io>
2019-01-02 20:05:45 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/demo/web/AxonMongoDemoRestController.java
@@ -0,0 +1,20 @@
+package com.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AxonMongoDemoRestController {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Hello, world! Add /hello/there to the URL to get a friendly reply.";
+    }
+
+}
