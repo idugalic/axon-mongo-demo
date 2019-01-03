74688cd55eda6b5cd65395c228fce692a827aabf
Ivan Dugalic <ivan.dugalic@axoniq.io>
2019-01-03 18:13:06 +0100

    Configure EmbeddedEventStore

--- src/main/java/com/demo/AxonMongoDemoApplication.java
+++ src/main/java/com/demo/AxonMongoDemoApplication.java
@@ -1,9 +1,12 @@
 package com.demo;
 
 import com.mongodb.MongoClient;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.spring.config.AxonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +18,29 @@ public class AxonMongoDemoApplication {
         SpringApplication.run(AxonMongoDemoApplication.class, args);
     }
 
+    /**
+     * Axon provides an event store `EmbeddedEventStore`. It delegates actual storage and retrieval of events to an `EventStorageEngine`.
+     *
+     * @param storageEngine
+     * @param configuration
+     * @return EmbeddedEventStore
+     */
     @Bean
-    public EventStorageEngine eventStore(MongoClient client) {
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration configuration) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
+                .build();
+    }
+
+    /**
+     * The MongoEventStorageEngine stores each event in a separate MongoDB document
+     *
+     * @param client
+     * @return EventStorageEngine
+     */
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient client) {
         return MongoEventStorageEngine.builder().mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build()).build();
     }
 }
